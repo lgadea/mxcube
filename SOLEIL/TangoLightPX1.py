da83b2e9f14a091ed7251adb02f9f8ab19ae784a
vrey01 <vrey01@gmail.com>
2015-03-16 13:04:11 +0100

    Implementation of using the PX1Environment device for the insertion of the light arm by the lightControlBrick.

--- SOLEIL/TangoLightPX1.py
+++ SOLEIL/TangoLightPX1.py
@@ -1,6 +1,7 @@
 import logging, time
-from qt import qApp
+from PyTango import DeviceProxy as dp
 from HardwareRepository.BaseHardwareObjects import Device
+#from PX1Environment import EnvironmentPhase
 
 class TangoLightPX1(Device):
 
@@ -21,14 +22,17 @@ class TangoLightPX1(Device):
         self.set_out = self.getCommandObject("set_out")
         
         # Avoiding Beamstop + Light Arm collision.
-        self.beamstop_out = self.getCommandObject("beamstop_out")
-        self.beamstopState = self.getChannelObject("beamstopState")
+        #self.beamstop_out = self.getCommandObject("beamstop_out")
+        #self.beamstopState = self.getChannelObject("beamstopState")
         
         # Avoiding Colision with the Detector
-        self.detdistchan = self.getChannelObject("detDistance")
-        logging.getLogger("HWR").info(('TangoLightPX1. minimum Detector '
-                        + 'distance for light_Arm insertion: %.1f mm') % \
-                                           self.min_detector_distance)
+        #self.detdistchan = self.getChannelObject("detDistance")
+        #logging.getLogger("HWR").info(('TangoLightPX1. minimum Detector '
+        #                + 'distance for light_Arm insertion: %.1f mm') % \
+        #                                   self.min_detector_distance)
+        self.environment = self.getObjectByRole("environment")
+        self.px1env = dp(self.px1environment_dev)
+        self.light = dp(self.light_dev)
 
         self._setReady()
         try:
@@ -66,34 +70,44 @@ class TangoLightPX1(Device):
         return self.currentState 
 
     def wagoIn(self):
-        if str(self.beamstopState.getValue()) == 'INSERT':
-            logging.getLogger("HWR").info('TangoLightPX1. Extracting BeamStop.')
-            self.beamstop_out()
-            t0 = time.time()
-            while str(self.beamstopState.getValue()) != "EXTRACT":
-                time.sleep(0.02)
-                qApp.processEvents()
-                if (time.time() - t0) > 5:
-                    logging.getLogger("HWR").info('TangoLightPX1. Time out while trying to extract the beamstop.')
-                    return
-            logging.getLogger("HWR").info('TangoLightPX1. Time to extract the beamstop: %.2f sec.' % (time.time()-t0))
-        detposition = self.detdistchan.getValue()
-        logging.getLogger("HWR").info('TangoLightPX1. DetDist= %.2f mm. OK.' % detposition)
-        if detposition < self.min_detector_distance:
-            m1 = "Can't insert Light-arm, detector distance too close: %.1f mm. " % detposition
-            m2 = "You need to set the distance to > %.1f mm." % self.min_detector_distance
-            logging.getLogger("user_level_log").error("%s: " + m1+m2, self.name())
-        else:
+        logging.getLogger("HWR").info('TangoLightPX1. GoToVisuSamplePhase.')
+        if not self.px1env.readyForVisuSample:
             logging.getLogger("HWR").info('TangoLightPX1. Inserting Light.')       
-            self.setIn()
+            self.px1env.GoToVisuSamplePhase()
+            debut = time.time()
+            while self.px1env.readyForVisuSample != True:
+                time.sleep(0.1)
+	        if (time.time() - debut) > 20:
+                   logging.debug("PX1Xanes - Timed out while going to FluoXPhase")
+	           break
+        #if str(self.beamstopState.getValue()) == 'INSERT':
+	    #self.beamstop_out()
+            #t0 = time.time()
+            #while str(self.beamstopState.getValue()) != "EXTRACT":
+            #    time.sleep(0.02)
+            #    qApp.processEvents()
+            #    if (time.time() - t0) > 5:
+            #        logging.getLogger("HWR").info('TangoLightPX1. Time out while trying to extract the beamstop.')
+            #        return
+            #logging.getLogger("HWR").info('TangoLightPX1. Time to extract the beamstop: %.2f sec.' % (time.time()-t0))
+        #detposition = self.detdistchan.getValue()
+        #logging.getLogger("HWR").info('TangoLightPX1. DetDist= %.2f mm. OK.' % detposition)
+        #if detposition < self.min_detector_distance:
+        #    m1 = "Can't insert Light-arm, detector distance too close: %.1f mm. " % detposition
+        #    m2 = "You need to set the distance to > %.1f mm." % self.min_detector_distance
+        #    logging.getLogger("user_level_log").error("%s: " + m1+m2, self.name())
+        #else:
+        #    logging.getLogger("HWR").info('TangoLightPX1. Inserting Light.')       
+        #    self.setIn()
 
     def setIn(self):
         self._setReady()
         if self.isReady():
           if self.inversed:
-            self.set_out()
+	      self.light.intensity = 0.
+              self.set_out()
           else:
-            self.set_in()
+              self.set_in()
  
     def wagoOut(self):
         logging.getLogger("HWR").info('TangoLightPX1:  in WagoOut ')
@@ -103,7 +117,8 @@ class TangoLightPX1(Device):
         self._setReady()
         if self.isReady():
           if self.inversed:
-            self.set_in()
+              self.set_in()
           else:
-            self.set_out()
+ 	      self.light.intensity = 0.
+              self.set_out()
            
